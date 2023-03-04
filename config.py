d9df7d2d8fcf1993aee964d6260adb9763bbcba1
MR-KASHMIRI-HACKER <74145014+MR-KASHMIRI-HACKER@users.noreply.github.com>
2023-03-04 18:37:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 DOWNLOAD_LOCATION = "./Downloads"
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "asddsadsd")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6258951148:AAGR2qjFggeRJF9c59R1wKOgIe08HkdEuRI")
 # BOT_TOKEN = "894269743:AAH3OAtoNulAMPwJg-Cp4zAGBxGazTlzn38"  #  @aryan_vbot
-APP_ID = int(os.environ.get("API_ID", "sdasdsd"))
-API_HASH = os.environ.get("API_HASH", "dsfdfsf")
+APP_ID = int(os.environ.get("API_ID", "27089583"))
+API_HASH = os.environ.get("API_HASH", "71c753bdfdaca0976cd411fd021a4d91")
 AUTH_GROUP = ""
 
 Creds_path = "creds"
